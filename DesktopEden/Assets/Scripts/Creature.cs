d8d498f426baa65f68f7fc6844fc2c43516e1f25
Scott King <jsking@andrew.cmu.edu>
2023-02-13 15:35:29 -0500

    Push missed file

--- DesktopEden/Assets/Scripts/Creature.cs
+++ DesktopEden/Assets/Scripts/Creature.cs
@@ -6,13 +6,22 @@ public class Creature : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     
-    [Header("Body Parts")]
+    [Header("Body Part Objects")]
     [SerializeField] private List<GameObject> _hats = new List<GameObject>();
     [SerializeField] private List<GameObject> _mouth = new List<GameObject>();
     [SerializeField] private List<GameObject> _eyes = new List<GameObject>();
     [SerializeField] private List<GameObject> _head = new List<GameObject>();
     [SerializeField] private List<GameObject> _legs = new List<GameObject>();
     [SerializeField] private List<GameObject> _body = new List<GameObject>();
+
+    [Header("Traits")]
+    // Indexes are relative to the list of associated game objects; -1 means that body part is missing
+    public int hatIndex = -1;
+    public int mouthIndex = -1;
+    public int eyesIndex = -1;
+    public int headIndex = -1;
+    public int legsIndex = -1;
+    public int bodyIndex = -1;
 
     public void SetSprites(int hatIndex, int mouthIndex, int eyesIndex, int headIndex, int legsIndex, int bodyIndex)
     {
